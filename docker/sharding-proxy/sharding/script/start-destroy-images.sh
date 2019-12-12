0c6305b6d71409b28fb28dd68cbde83f4fb2b451
WangHaitao <46469719+haetao@users.noreply.github.com>
2019-12-12 14:28:11 +0800

    docker (#6)

--- docker/sharding-proxy/sharding/script/start-destroy-images.sh
+++ docker/sharding-proxy/sharding/script/start-destroy-images.sh
@@ -2,6 +2,6 @@
 cd docker
 sudo rm -f sharding-proxy-bin.tar.gz
 cd sharding-proxy/sharding
-#sudo docker-compose down --rmi all
+#sudo docker-compose down --rmi all remove all images of service
 sudo docker-compose down
 cd ../../../
