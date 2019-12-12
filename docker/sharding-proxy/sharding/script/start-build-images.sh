e781b5cb0c45e782e1beff7a65740eecaa35cf2d
WangHaitao <46469719+haetao@users.noreply.github.com>
2019-12-12 20:22:25 +0800

    Mstaer docker (#10)
    * adjust script

--- docker/sharding-proxy/sharding/script/start-build-images.sh
+++ docker/sharding-proxy/sharding/script/start-build-images.sh
@@ -5,7 +5,7 @@ sudo cp ../../$1/sharding-distribution/sharding-proxy-distribution/target/*.tar.
 cd sharding-proxy/sharding
 sudo docker-compose up -d
 cd ../../../
-sudo sed -i 's/localhost:3306/${ip}:3309/g' `grep localhost:3306 -rl sharding-jdbc-example/`
-sudo sed -i 's/localhost:3307/${ip}:3308/g' `grep localhost:3307 -rl sharding-proxy-example/`
+sudo sed -i "s/localhost:3306/${ip}:3309/g" `grep localhost:3306 -rl sharding-jdbc-example/`
+sudo sed -i "s/localhost:3307/${ip}:3308/g" `grep localhost:3307 -rl sharding-proxy-example/`
 sudo chmod 777 docker/tools/wait-for-it.sh
 sudo bash docker/tools/wait-for-it.sh ${ip}:3308 -- echo "sharding-proxy is available!"
