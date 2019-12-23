4194e71caf33ea019303af2c37db70ddc7529caa
wanghiato71 <haetao@outlook.com>
2019-12-23 11:01:16 +0800

    modify sql

--- src/resources/manual_schema.sql
+++ src/resources/manual_schema.sql
@@ -63,5 +63,3 @@ CREATE SCHEMA IF NOT EXISTS demo_ds_master_1_slave_1;
  CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_0.t_order_item_1 (order_item_id BIGINT NOT NULL AUTO_INCREMENT, order_id BIGINT NOT NULL, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_item_id));
  CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_1.t_order_item_0 (order_item_id BIGINT NOT NULL AUTO_INCREMENT, order_id BIGINT NOT NULL, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_item_id));
  CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_1.t_order_item_1 (order_item_id BIGINT NOT NULL AUTO_INCREMENT, order_id BIGINT NOT NULL, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_item_id));
-
- set global max_connections=1000;
