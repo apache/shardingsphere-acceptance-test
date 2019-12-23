cff141fb86d492335f1ed00a0db4fb210f658e7e
wanghiato71 <haetao@outlook.com>
2019-12-23 12:22:46 +0800

    modify docker

--- docker/script/build-images.sh
+++ docker/script/build-images.sh
@@ -2,10 +2,10 @@
 cd docker
 ip=`ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:" | head -1`
 sudo cp ../../$1/sharding-distribution/sharding-proxy-distribution/target/*.tar.gz  ./sharding-proxy-bin.tar.gz
-cd sharding-jdbc/sharding
+cd sharding-jdbc/jdbc
 sudo docker-compose up -d
 cd ../../
-cd sharding-proxy/sharding
+cd sharding-proxy/proxy
 sudo docker-compose up -d
 cd ../../../
 sudo sed -i "s/localhost:3306/${ip}:3310/g" `grep localhost:3306 -rl sharding-jdbc-example/`
--- docker/script/destroy-images.sh
+++ docker/script/destroy-images.sh
@@ -2,11 +2,11 @@
 pwd
 cd docker
 sudo rm -f sharding-proxy-bin.tar.gz
-cd sharding-jdbc/sharding
+cd sharding-jdbc/jdbc
 #sudo docker-compose down --rmi all remove all images of service
 sudo docker-compose down
 cd ../../
-cd sharding-proxy/sharding
+cd sharding-proxy/proxy
 #sudo docker-compose down --rmi all remove all images of service
 sudo docker-compose down
 cd ../../../
