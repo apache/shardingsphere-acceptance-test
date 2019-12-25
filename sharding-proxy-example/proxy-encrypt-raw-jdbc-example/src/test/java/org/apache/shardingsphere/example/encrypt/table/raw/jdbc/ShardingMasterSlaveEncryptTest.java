d73b1e247c6fd82b5b05f95903224c2f0caaf68d
geomon <1623537355@qq.com>
2019-12-25 12:14:19 +0800

    update proxy

---
+++ sharding-proxy-example/proxy-encrypt-raw-jdbc-example/src/test/java/org/apache/shardingsphere/example/encrypt/table/raw/jdbc/ShardingMasterSlaveEncryptTest.java
@@ -0,0 +1,31 @@
+package org.apache.shardingsphere.example.encrypt.table.raw.jdbc;
+
+
+import org.apache.shardingsphere.example.core.api.DataSourceUtil;
+import org.apache.shardingsphere.example.core.api.ExampleExecuteTemplate;
+import org.apache.shardingsphere.example.core.api.service.ExampleService;
+import org.apache.shardingsphere.example.core.jdbc.common.SpringBootRawJdbcAssertUtils;
+import org.apache.shardingsphere.example.core.jdbc.repository.UserRepositoryImpl;
+import org.apache.shardingsphere.example.core.jdbc.service.UserServiceImpl;
+import org.junit.Test;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+/**
+ * @author linzesi
+ */
+public class ShardingMasterSlaveEncryptTest {
+    
+    private static ExampleService getExampleService(final DataSource dataSource) {
+        return new UserServiceImpl(new UserRepositoryImpl(dataSource));
+    }
+    
+    @Test
+    public void assertEncryptDatabases() throws SQLException {
+        DataSource dataSource = DataSourceUtil.createDataSourceEncrypt("sharding_master_slave_encrypt_db");
+        ExampleService exampleService = getExampleService(dataSource);
+        ExampleExecuteTemplate.run(exampleService);
+        SpringBootRawJdbcAssertUtils.assertShardingMasterSlaveEncrypt(exampleService);
+    }
+}
--- sharding-proxy-example/sharding-proxy-boot-mybatis-example/src/test/java/org/apache/shardingsphere/example/proxy/spring/boot/mybatis/SpringBootEncryptTest.java
+++ sharding-proxy-example/sharding-proxy-boot-mybatis-example/src/test/java/org/apache/shardingsphere/example/proxy/spring/boot/mybatis/SpringBootEncryptTest.java
@@ -1,12 +1,15 @@
 package org.apache.shardingsphere.example.proxy.spring.boot.mybatis;
 
-import org.apache.shardingsphere.example.core.api.senario.AnnotationCommonServiceScenario;
-import org.apache.shardingsphere.example.core.mybatis.common.SpringResultAssertUtils;
+import org.apache.shardingsphere.example.core.api.ExampleExecuteTemplate;
+import org.apache.shardingsphere.example.core.api.service.ExampleService;
 import org.apache.shardingsphere.example.core.mybatis.service.SpringPojoService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -19,10 +22,35 @@ public class SpringBootEncryptTest {
     
     @Autowired
     private SpringPojoService commonService;
+    @Autowired
+    @Qualifier("encrypt")
+    private ExampleService encrypt;
+    
+    private static void process(final ConfigurableApplicationContext applicationContext) throws SQLException {
+        ExampleService exampleService = getExampleService(applicationContext);
+        exampleService.initEnvironment();
+        exampleService.processSuccess();
+        try {
+            exampleService.processFailure();
+        } catch (final Exception ex) {
+            System.out.println(ex.getMessage());
+            exampleService.printData();
+        } finally {
+            exampleService.cleanEnvironment();
+        }
+    }
+    
+    private static ExampleService getExampleService(final ConfigurableApplicationContext applicationContext) {
+        return applicationContext.getBean("encrypt",ExampleService.class);
+    }
     
     @Test
     public void assertCommonService() throws SQLException {
-        AnnotationCommonServiceScenario.process1(commonService);
-        SpringResultAssertUtils.assertEncryptResult(commonService);
+//        ExampleExecuteTemplate.run(encrypt);
+//        AnnotationCommonServiceScenario.process1(commonService);
+//        SpringResultAssertUtils.assertEncryptResult(commonService);
+//        try (ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootTestMain.class)) {
+            ExampleExecuteTemplate.run(encrypt);
+//        }
     }
 }
