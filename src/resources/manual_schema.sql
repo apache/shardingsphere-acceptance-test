4362a146db8b7221011700abeb04ef7fa8764dc3
geomon <1623537355@qq.com>
2019-12-26 17:32:00 +0800

    update schema.sql

--- src/resources/manual_schema.sql
+++ src/resources/manual_schema.sql
@@ -29,6 +29,14 @@ CREATE SCHEMA IF NOT EXISTS demo_ds_master_1_slave_0;
 CREATE SCHEMA IF NOT EXISTS demo_ds_master_1_slave_1;
 
 -- Should sync from master-salve automatically
+ CREATE TABLE IF NOT EXISTS demo_ds_0.t_user_0 (user_id INT NOT NULL AUTO_INCREMENT, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd VARCHAR(200), PRIMARY 
+ KEY (user_id));
+ CREATE TABLE IF NOT EXISTS demo_ds_0.t_user_1 (user_id INT NOT NULL AUTO_INCREMENT, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd VARCHAR(200), PRIMARY 
+ KEY (user_id));
+ CREATE TABLE IF NOT EXISTS demo_ds_1.t_user_0 (user_id INT NOT NULL AUTO_INCREMENT, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd VARCHAR(200), PRIMARY 
+ KEY (user_id));
+ CREATE TABLE IF NOT EXISTS demo_ds_1.t_user_1 (user_id INT NOT NULL AUTO_INCREMENT, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd VARCHAR(200), PRIMARY 
+ KEY (user_id));
  CREATE TABLE IF NOT EXISTS demo_ds_slave_0.t_order (order_id BIGINT NOT NULL AUTO_INCREMENT, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_id));
  CREATE TABLE IF NOT EXISTS demo_ds_slave_1.t_order (order_id BIGINT NOT NULL AUTO_INCREMENT, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_id));
  CREATE TABLE IF NOT EXISTS demo_ds_slave_0.t_order_item (order_item_id BIGINT NOT NULL AUTO_INCREMENT, order_id BIGINT NOT NULL, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_item_id));
