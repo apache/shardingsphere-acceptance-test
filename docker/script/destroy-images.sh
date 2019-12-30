bdda19a2d615c152f9630bb7a65cc3b82f7ee9ce
wanghiato71 <haetao@outlook.com>
2019-12-30 18:09:27 +0800

    Adjust destroy shell

--- docker/script/destroy-images.sh
+++ docker/script/destroy-images.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env sh
-pwd
 cd docker
 sudo rm -f sharding-proxy-bin.tar.gz
 cd sharding-jdbc/jdbc
-#sudo docker-compose down --rmi all remove all images of service
-sudo docker-compose down
+#sudo docker-compose down --rmi all //remove all images of service
+if [[ $bool == true ]] ; then
+  sudo docker-compose down --rmi
+elif [[ $bool == false || $bool == "" ]] ; then
+  sudo docker-compose down
+fi
 cd ../../
 cd sharding-proxy/proxy
-#sudo docker-compose down --rmi all remove all images of service
-sudo docker-compose down
+#sudo docker-compose down --rmi all //remove all images of service
+if [[ $bool == true ]] ; then
+  sudo docker-compose down --rmi
+elif [[ $bool == false || $bool == "" ]] ; then
+  sudo docker-compose down
+fi
 cd ../../../
