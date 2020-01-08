e51dbe2f5edb7f33f5eea7dad24e6e035827e024
geomon <1623537355@qq.com>
2020-01-08 11:26:10 +0800

    update pg sql

--- src/resources/manual_schema_postgre.sql
+++ src/resources/manual_schema_postgre.sql
@@ -31,20 +31,22 @@ CREATE SCHEMA IF NOT EXISTS demo_ds_master_1_slave_0;
 CREATE SCHEMA IF NOT EXISTS demo_ds_master_1_slave_1;
 
 -- Should sync from master-salve automatically
- CREATE TABLE IF NOT EXISTS demo_ds_0.t_user_0 (user_id SERIAL NOT NULL, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd VARCHAR(200), PRIMARY 
- KEY (user_id));
- CREATE TABLE IF NOT EXISTS demo_ds_0.t_user_1 (user_id SERIAL NOT NULL, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd VARCHAR(200), PRIMARY 
- KEY (user_id));
- CREATE TABLE IF NOT EXISTS demo_ds_1.t_user_0 (user_id SERIAL NOT NULL, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd VARCHAR(200), PRIMARY 
- KEY (user_id));
- CREATE TABLE IF NOT EXISTS demo_ds_1.t_user_1 (user_id SERIAL NOT NULL, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd VARCHAR(200), PRIMARY 
- KEY (user_id));
+ CREATE TABLE IF NOT EXISTS demo_ds_0.t_user_0 (user_id BIGSERIAL NOT NULL, user_name VARCHAR(200), user_name_cipher VARCHAR(200), pwd VARCHAR(200), pwd_cipher VARCHAR(200), pwd_plain VARCHAR(200),
+  PRIMARY KEY (user_id));
+ CREATE TABLE IF NOT EXISTS demo_ds_0.t_user_1 (user_id BIGSERIAL NOT NULL, user_name VARCHAR(200), user_name_cipher VARCHAR(200), pwd VARCHAR(200), pwd_cipher VARCHAR(200), pwd_plain VARCHAR(200),
+  PRIMARY KEY (user_id));
+ CREATE TABLE IF NOT EXISTS demo_ds_1.t_user_0 (user_id BIGSERIAL NOT NULL, user_name VARCHAR(200), user_name_cipher VARCHAR(200), pwd VARCHAR(200), pwd_cipher VARCHAR(200), pwd_plain VARCHAR(200),
+  PRIMARY KEY (user_id));
+ CREATE TABLE IF NOT EXISTS demo_ds_1.t_user_1 (user_id BIGSERIAL NOT NULL, user_name VARCHAR(200), user_name_cipher VARCHAR(200), pwd VARCHAR(200), pwd_cipher VARCHAR(200), pwd_plain VARCHAR(200),
+  PRIMARY KEY (user_id));
  CREATE TABLE IF NOT EXISTS demo_ds_slave_0.t_order (order_id SERIAL NOT NULL, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_id));
  CREATE TABLE IF NOT EXISTS demo_ds_slave_1.t_order (order_id SERIAL NOT NULL, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_id));
  CREATE TABLE IF NOT EXISTS demo_ds_slave_0.t_order_item (order_item_id SERIAL NOT NULL, order_id BIGINT NOT NULL, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_item_id));
  CREATE TABLE IF NOT EXISTS demo_ds_slave_1.t_order_item (order_item_id SERIAL NOT NULL, order_id BIGINT NOT NULL, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_item_id));
- CREATE TABLE IF NOT EXISTS demo_ds_slave_0.t_user (user_id SERIAL NOT NULL, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd VARCHAR(200), PRIMARY KEY (user_id));
- CREATE TABLE IF NOT EXISTS demo_ds_slave_1.t_user (user_id SERIAL NOT NULL, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd VARCHAR(200), PRIMARY KEY (user_id));
+ CREATE TABLE IF NOT EXISTS demo_ds_slave_0.t_user (user_id BIGSERIAL NOT NULL, user_name VARCHAR(200), user_name_cipher VARCHAR(200), pwd VARCHAR(200), pwd_cipher VARCHAR(200), pwd_plain VARCHAR(200),
+                                                    PRIMARY KEY (user_id));
+ CREATE TABLE IF NOT EXISTS demo_ds_slave_1.t_user (user_id BIGSERIAL NOT NULL, user_name VARCHAR(200), user_name_cipher VARCHAR(200), pwd VARCHAR(200), pwd_cipher VARCHAR(200), pwd_plain VARCHAR(200),
+                                                    PRIMARY KEY (user_id));
  CREATE TABLE IF NOT EXISTS demo_ds_slave_0.t_address (address_id BIGINT NOT NULL, address_name VARCHAR(100) NOT NULL, PRIMARY KEY (address_id));
  CREATE TABLE IF NOT EXISTS demo_ds_slave_0.t_address (address_id BIGINT NOT NULL, address_name VARCHAR(100) NOT NULL, PRIMARY KEY (address_id));
 
@@ -56,28 +58,33 @@ CREATE SCHEMA IF NOT EXISTS demo_ds_master_1_slave_1;
  CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_0.t_order_1 (order_id SERIAL NOT NULL, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_id));
  CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_1.t_order_0 (order_id SERIAL NOT NULL, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_id));
  CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_1.t_order_1 (order_id SERIAL NOT NULL, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_id));
- CREATE TABLE IF NOT EXISTS demo_ds_master_0_slave_0.t_user (user_id SERIAL NOT NULL, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd VARCHAR(200), PRIMARY KEY (user_id));
- CREATE TABLE IF NOT EXISTS demo_ds_master_0_slave_1.t_user (user_id SERIAL NOT NULL, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd VARCHAR(200), PRIMARY KEY (user_id));
+ CREATE TABLE IF NOT EXISTS demo_ds_master_0_slave_0.t_user (user_id BIGSERIAL NOT NULL, user_name VARCHAR(200), user_name_cipher VARCHAR(200), pwd VARCHAR(200), pwd_cipher VARCHAR(200), pwd_plain VARCHAR(200),
+                                                             PRIMARY KEY (user_id));
+ CREATE TABLE IF NOT EXISTS demo_ds_master_0_slave_1.t_user (user_id BIGSERIAL NOT NULL, user_name VARCHAR(200), user_name_cipher VARCHAR(200), pwd VARCHAR(200), pwd_cipher VARCHAR(200), pwd_plain VARCHAR(200),
+                                                             PRIMARY KEY (user_id));
  CREATE TABLE IF NOT EXISTS demo_ds_master_0_slave_0.t_address (address_id BIGINT NOT NULL, address_name VARCHAR(100) NOT NULL, PRIMARY KEY (address_id));
  CREATE TABLE IF NOT EXISTS demo_ds_master_0_slave_1.t_address (address_id BIGINT NOT NULL, address_name VARCHAR(100) NOT NULL, PRIMARY KEY (address_id));
- CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_0.t_user (user_id SERIAL NOT NULL, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd VARCHAR(200), PRIMARY KEY (user_id));
+ CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_0.t_user (user_id BIGSERIAL NOT NULL, user_name VARCHAR(200), user_name_cipher VARCHAR(200), pwd VARCHAR(200), pwd_cipher VARCHAR(200), pwd_plain VARCHAR(200),
+                                                             PRIMARY KEY (user_id));
  
- CREATE TABLE IF NOT EXISTS demo_ds_master_0_slave_0.t_user_0 (user_id SERIAL NOT NULL, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd 
- VARCHAR(200), PRIMARY KEY (user_id));
- CREATE TABLE IF NOT EXISTS demo_ds_master_0_slave_0.t_user_1 (user_id SERIAL NOT NULL, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd 
- VARCHAR(200), PRIMARY KEY (user_id));
- CREATE TABLE IF NOT EXISTS demo_ds_master_0_slave_1.t_user_0 (user_id SERIAL NOT NULL, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd 
- VARCHAR(200), PRIMARY KEY (user_id));
- CREATE TABLE IF NOT EXISTS demo_ds_master_0_slave_1.t_user_1 (user_id SERIAL NOT NULL, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd 
- VARCHAR(200), PRIMARY KEY (user_id));
- CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_0.t_user_0 (user_id SERIAL NOT NULL, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd VARCHAR(200), PRIMARY KEY (user_id));
- CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_0.t_user_1 (user_id SERIAL NOT NULL, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd 
- VARCHAR(200), PRIMARY KEY (user_id));
- CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_1.t_user_0 (user_id SERIAL NOT NULL, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd 
- VARCHAR(200), PRIMARY KEY (user_id));
- CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_1.t_user_1 (user_id SERIAL NOT NULL, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd 
- VARCHAR(200), PRIMARY KEY (user_id));
- CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_1.t_user (user_id SERIAL NOT NULL, user_name VARCHAR(200), user_name_plain VARCHAR(200), pwd VARCHAR(200), assisted_query_pwd VARCHAR(200), PRIMARY KEY (user_id));
+ CREATE TABLE IF NOT EXISTS demo_ds_master_0_slave_0.t_user_0 (user_id BIGSERIAL NOT NULL, user_name VARCHAR(200), user_name_cipher VARCHAR(200), pwd VARCHAR(200), pwd_cipher VARCHAR(200), pwd_plain VARCHAR(200),
+                                                               PRIMARY KEY (user_id));
+ CREATE TABLE IF NOT EXISTS demo_ds_master_0_slave_0.t_user_1 (user_id BIGSERIAL NOT NULL, user_name VARCHAR(200), user_name_cipher VARCHAR(200), pwd VARCHAR(200), pwd_cipher VARCHAR(200), pwd_plain VARCHAR(200),
+                                                               PRIMARY KEY (user_id));
+ CREATE TABLE IF NOT EXISTS demo_ds_master_0_slave_1.t_user_0 (user_id BIGSERIAL NOT NULL, user_name VARCHAR(200), user_name_cipher VARCHAR(200), pwd VARCHAR(200), pwd_cipher VARCHAR(200), pwd_plain VARCHAR(200),
+                                                               PRIMARY KEY (user_id));
+ CREATE TABLE IF NOT EXISTS demo_ds_master_0_slave_1.t_user_1 (user_id BIGSERIAL NOT NULL, user_name VARCHAR(200), user_name_cipher VARCHAR(200), pwd VARCHAR(200), pwd_cipher VARCHAR(200), pwd_plain VARCHAR(200),
+                                                               PRIMARY KEY (user_id));
+ CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_0.t_user_0 (user_id BIGSERIAL NOT NULL, user_name VARCHAR(200), user_name_cipher VARCHAR(200), pwd VARCHAR(200), pwd_cipher VARCHAR(200), pwd_plain VARCHAR(200),
+                                                               PRIMARY KEY (user_id));
+ CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_0.t_user_1 (user_id BIGSERIAL NOT NULL, user_name VARCHAR(200), user_name_cipher VARCHAR(200), pwd VARCHAR(200), pwd_cipher VARCHAR(200), pwd_plain VARCHAR(200),
+                                                               PRIMARY KEY (user_id));
+ CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_1.t_user_0 (user_id BIGSERIAL NOT NULL, user_name VARCHAR(200), user_name_cipher VARCHAR(200), pwd VARCHAR(200), pwd_cipher VARCHAR(200), pwd_plain VARCHAR(200),
+                                                               PRIMARY KEY (user_id));
+ CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_1.t_user_1 (user_id BIGSERIAL NOT NULL, user_name VARCHAR(200), user_name_cipher VARCHAR(200), pwd VARCHAR(200), pwd_cipher VARCHAR(200), pwd_plain VARCHAR(200),
+                                                               PRIMARY KEY (user_id));
+ CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_1.t_user (user_id BIGSERIAL NOT NULL, user_name VARCHAR(200), user_name_cipher VARCHAR(200), pwd VARCHAR(200), pwd_cipher VARCHAR(200), pwd_plain VARCHAR(200),
+                                                             PRIMARY KEY (user_id));
  CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_0.t_address (address_id BIGINT NOT NULL, address_name VARCHAR(100) NOT NULL, PRIMARY KEY (address_id));
  CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_1.t_address (address_id BIGINT NOT NULL, address_name VARCHAR(100) NOT NULL, PRIMARY KEY (address_id));
  CREATE TABLE IF NOT EXISTS demo_ds_master_0_slave_0.t_order_item_0 (order_item_id SERIAL NOT NULL, order_id BIGINT NOT NULL, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_item_id));
