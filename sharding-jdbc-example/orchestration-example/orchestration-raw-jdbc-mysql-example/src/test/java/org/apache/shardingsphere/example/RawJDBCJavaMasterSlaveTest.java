474bb6d8831396be3308bec06c9e202446c7ce87
haetao <haetao@outlook.com>
2020-01-10 11:18:01 +0800

    add orch raw jdbc mysql test

---
+++ sharding-jdbc-example/orchestration-example/orchestration-raw-jdbc-mysql-example/src/test/java/org/apache/shardingsphere/example/RawJDBCJavaMasterSlaveTest.java
@@ -0,0 +1,25 @@
+package org.apache.shardingsphere.example;
+
+import org.apache.shardingsphere.example.core.api.ExampleExecuteTemplate;
+import org.apache.shardingsphere.example.core.api.service.ExampleService;
+import org.apache.shardingsphere.example.core.jdbc.common.RawJdbcAssertUtils;
+import org.apache.shardingsphere.example.core.jdbc.repository.mysql.AddressRepositoryImpl;
+import org.apache.shardingsphere.example.core.jdbc.repository.mysql.OrderItemRepositoryImpl;
+import org.apache.shardingsphere.example.core.jdbc.repository.mysql.OrderRepositoryImpl;
+import org.apache.shardingsphere.example.core.jdbc.service.OrderServiceImpl;
+import org.apache.shardingsphere.example.factory.DataSourceFactory;
+import org.apache.shardingsphere.example.type.ShardingType;
+import org.junit.Test;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+public class RawJDBCJavaMasterSlaveTest {
+    @Test
+    public void assertCommonService() throws SQLException {
+        DataSource dataSource = DataSourceFactory.newInstance(ShardingType.MASTER_SLAVE);
+        ExampleService exampleService = new OrderServiceImpl(new OrderRepositoryImpl(dataSource),new OrderItemRepositoryImpl(dataSource), new AddressRepositoryImpl(dataSource));
+        ExampleExecuteTemplate.run(exampleService);
+        RawJdbcAssertUtils.assertMasterSlaves(exampleService);
+    }
+}
---
+++ sharding-jdbc-example/orchestration-example/orchestration-raw-jdbc-mysql-example/src/test/java/org/apache/shardingsphere/example/config/ShardingMasterSlaveEncryptConfiguration.java
@@ -0,0 +1,91 @@
+package org.apache.shardingsphere.example.config;
+
+import com.google.common.collect.Lists;
+import org.apache.shardingsphere.api.config.masterslave.MasterSlaveRuleConfiguration;
+import org.apache.shardingsphere.api.config.sharding.ShardingRuleConfiguration;
+import org.apache.shardingsphere.api.config.sharding.TableRuleConfiguration;
+import org.apache.shardingsphere.api.config.sharding.strategy.InlineShardingStrategyConfiguration;
+import org.apache.shardingsphere.encrypt.api.EncryptColumnRuleConfiguration;
+import org.apache.shardingsphere.encrypt.api.EncryptRuleConfiguration;
+import org.apache.shardingsphere.encrypt.api.EncryptTableRuleConfiguration;
+import org.apache.shardingsphere.encrypt.api.EncryptorRuleConfiguration;
+import org.apache.shardingsphere.example.core.api.DataSourceUtil;
+import org.apache.shardingsphere.example.core.api.DatabaseType;
+import org.apache.shardingsphere.orchestration.config.OrchestrationConfiguration;
+import org.apache.shardingsphere.orchestration.reg.api.RegistryCenterConfiguration;
+import org.apache.shardingsphere.shardingjdbc.api.ShardingDataSourceFactory;
+import org.apache.shardingsphere.shardingjdbc.orchestration.api.OrchestrationShardingDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.*;
+
+public class ShardingMasterSlaveEncryptConfiguration implements ExampleConfiguration{
+    @Override
+    public DataSource getDataSource() throws SQLException {
+        ShardingRuleConfiguration shardingRuleConfig = new ShardingRuleConfiguration();
+        shardingRuleConfig.getTableRuleConfigs().add(getTableRuleConfiguration());
+        shardingRuleConfig.setMasterSlaveRuleConfigs(getMasterSlaveRuleConfigurations());
+        shardingRuleConfig.setEncryptRuleConfig(getEncryptRuleConfiguration());
+        return OrchestrationShardingDataSourceFactory.createDataSource(
+                createDataSourceMap()
+                , shardingRuleConfig
+                , new Properties()
+                ,new OrchestrationConfiguration("orchestration-mysql-sharding-MS-encrypt",getRegistryCenterConfiguration(),true));
+    }
+
+    private static TableRuleConfiguration getTableRuleConfiguration() {
+        TableRuleConfiguration result = new TableRuleConfiguration("t_user", "ds_${0..1}.t_user_${[0, 1]}");
+        result.setDatabaseShardingStrategyConfig(new InlineShardingStrategyConfiguration("user_id", "ds_${user_id % 2}"));
+        result.setTableShardingStrategyConfig(new InlineShardingStrategyConfiguration("user_id","t_user_${user_id % 2}"));
+        return result;
+    }
+
+    private static List<MasterSlaveRuleConfiguration> getMasterSlaveRuleConfigurations() {
+        MasterSlaveRuleConfiguration masterSlaveRuleConfig1 = new MasterSlaveRuleConfiguration("ds_0", "demo_ds_master_0", Arrays.asList("demo_ds_master_0_slave_0", "demo_ds_master_0_slave_1"));
+        MasterSlaveRuleConfiguration masterSlaveRuleConfig2 = new MasterSlaveRuleConfiguration("ds_1", "demo_ds_master_1", Arrays.asList("demo_ds_master_1_slave_0", "demo_ds_master_1_slave_1"));
+        return Lists.newArrayList(masterSlaveRuleConfig1, masterSlaveRuleConfig2);
+    }
+
+    private EncryptRuleConfiguration getEncryptRuleConfiguration() {
+        Properties props = new Properties();
+        props.setProperty("aes.key.value", "123456");
+        EncryptorRuleConfiguration encryptorAES = new EncryptorRuleConfiguration("AES", props);
+        EncryptorRuleConfiguration encryptorMD5 = new EncryptorRuleConfiguration("MD5",new Properties());
+        Map<String, EncryptColumnRuleConfiguration> columns = new HashMap<>();
+        EncryptColumnRuleConfiguration columnUserName = new EncryptColumnRuleConfiguration("user_name", "user_name_cipher", "", "aes");
+        EncryptColumnRuleConfiguration columnPwd = new EncryptColumnRuleConfiguration("pwd_plain", "pwd_cipher", "", "md5");
+        columns.put("user_name",columnUserName);
+        columns.put("pwd",columnPwd);
+        EncryptTableRuleConfiguration tableConfig = new EncryptTableRuleConfiguration(columns);
+        EncryptRuleConfiguration result = new EncryptRuleConfiguration();
+        result.getEncryptors().put("aes", encryptorAES);
+        result.getEncryptors().put("md5",encryptorMD5);
+        result.getTables().put("t_user", tableConfig);
+        return result;
+    }
+
+    private static Map<String, DataSource> createDataSourceMap() {
+        final Map<String, DataSource> result = new HashMap<>();
+        result.put("demo_ds_master_0", DataSourceUtil.createDataSource("demo_ds_master_0", DatabaseType.MYSQL));
+        result.put("demo_ds_master_0_slave_0", DataSourceUtil.createDataSource("demo_ds_master_0_slave_0", DatabaseType.MYSQL));
+        result.put("demo_ds_master_0_slave_1", DataSourceUtil.createDataSource("demo_ds_master_0_slave_1", DatabaseType.MYSQL));
+        result.put("demo_ds_master_1", DataSourceUtil.createDataSource("demo_ds_master_1", DatabaseType.MYSQL));
+        result.put("demo_ds_master_1_slave_0", DataSourceUtil.createDataSource("demo_ds_master_1_slave_0", DatabaseType.MYSQL));
+        result.put("demo_ds_master_1_slave_1", DataSourceUtil.createDataSource("demo_ds_master_1_slave_1", DatabaseType.MYSQL));
+        return result;
+    }
+
+    private RegistryCenterConfiguration getRegistryCenterConfiguration() {
+        RegistryCenterConfiguration regConfig = new RegistryCenterConfiguration("zookeeper");
+        regConfig.setServerLists("localhost:2181");
+        regConfig.setNamespace("orchestration-raw-jdbc-mysql");
+        return regConfig;
+    }
+
+    private static Properties getProperties() {
+        Properties result = new Properties();
+        result.setProperty("worker.id", "123");
+        return result;
+    }
+}
