97902fcc6d4ce2a7b32ab631505eb622253bac92
haetao <haetao@outlook.com>
2020-01-14 16:03:57 +0800

    adjust shell

--- docker/script/build-images.sh
+++ docker/script/build-images.sh
@@ -28,8 +28,8 @@ cd ../../../
 sudo sed -i "s/localhost/${ip}/g" `grep localhost -rl example-core/example-api/`
 sudo sed -i "s/3306/${PORT_JDBC_MYSQL}/g" `grep 3306 -rl example-core/example-api/`
 sudo sed -i "s/5432/${PORT_JDBC_PG}/g" `grep 5432 -rl example-core/example-api/`
-sudo sed -i "s/3308/${PORT_PROXY_MYSQL_SHARDING}/g" `grep PROXY_MYSQL_PORT = ${PORT_RAW_PROXY} -rl example-core/example-api/`
-sudo sed -i "s/3309/${PORT_PROXY_PG_SHARDING}/g" `grep PROXY_POSTGRESQL_PORT = ${PORT_RAW_PROXY} -rl example-core/example-api/`
+sudo sed -i "s/3308/${PORT_PROXY_MYSQL_SHARDING}/g" `grep 3308 -rl example-core/example-api/`
+sudo sed -i "s/3309/${PORT_PROXY_PG_SHARDING}/g" `grep 3309 -rl example-core/example-api/`
 
 sudo sed -i "s/localhost:3306/${ip}:${PORT_JDBC_MYSQL}/g" `grep localhost:3306 -rl sharding-jdbc-example/`
 sudo sed -i "s/localhost:5432/${ip}:${PORT_JDBC_PG}/g" `grep localhost:5432 -rl sharding-jdbc-example/`
